aedd9b8e36e5b81e93a5614ad97398d26037b9d8
Aum Bhatt <aum@deriv.com>
2024-11-06 12:11:14 +0400

    refactor: replaced SimpleSubscriptionStore with Subscription for proposal

--- proposal.js
+++ proposal.js
@@ -1,46 +1,29 @@
 (function () {
     class ProposalStore {
 
-        constructor(wsClient, subscriptionStore, forget) {
+        constructor(wsClient, subscription) {
             this.wsClient = wsClient;
-            this.data = null;
-            this.currentSubscriptionID = null;
-            this.subscriptionStore = subscriptionStore;
-            this.forget = forget;
+            this.subscription = subscription;
         }
 
-        async subscribe(tradeParams, stateToUpdate) {
+        async subscribe(tradeParams, onDataHandler) {
             try {
-                await this.unsubscribe();
-
                 const payload = {
-                    ...tradeParams,
-                    proposal: 1,
+                    ...tradeParams
                 };
-
-                const callback = (response) => {
-                    if (stateToUpdate) {
-                        console.log("updating", stateToUpdate, "with proposal data");
-                        // update exposed bubble state from plugin
-                        window.bubbleInstance.publishState(stateToUpdate, jsonObjectsToBubbleThings(response));
-                    }
-                };
-
-                const response = await this.subscriptionStore.subscribe("a1-pRokL65OC3LA90AgZKegzalyl34MJ", payload, callback);
-                console.log(tradeParams)
-
-                this.currentSubscriptionID = response.subscription.id;
-
-                console.log(response)
+                /* 
+                    We do not store the subscriptionID return by subscribe as we
+                    unsubscribe to all `proposal` streams at ones.
+                */
+                await this.subscription.subscribe("proposal", payload, onDataHandler);
             } catch (error) {
-
+                throw error;
             }
         }
 
         async unsubscribe() {
             try {
-                const response = await this.forget.requestAll("proposal", "a1-pRokL65OC3LA90AgZKegzalyl34MJ");
-                return response;
+                await this.subscription.unsubscribeByStreamTypes(["proposal"]);
             } catch (error) {
                 throw error;
             }
@@ -49,13 +32,13 @@
 
 
     window.DerivData = window.DerivData || {};
-    window.DerivData.proposalStore = new ProposalStore(window.DerivData.simpleWS, window.DerivData.simpleSubscriptionStore, window.DerivData.forget);
+    window.DerivData.proposalStore = new ProposalStore(window.DerivData.simpleWS, window.DerivData.subscription);
 })()
 
 /* 
+
 {
     "proposal": 1,
-    "subscribe": 1,
     "amount": 10,
     "basis": "stake",
     "contract_type": "PUT",
@@ -63,33 +46,7 @@
     "symbol": "frxAUDJPY",
     "duration": 1,
     "duration_unit": "d",
-    "req_id": 19
 }
 
-
-
-{
-
-    "ask_price": 10,
-    "contract_details": {
-        "barrier": "100.729"
-    },
-    "date_expiry": 1730937599,
-    "date_start": 1730794767,
-    "display_value": "10.00",
-    "id": "018d03a0-af3b-aa71-fd4d-4ddf618826cb",
-    "longcode": "Win payout if AUD/JPY is strictly higher than entry spot at close on 2024-11-06.",
-    "payout": 17.33,
-    "spot": 100.729,
-    "spot_time": 1730794766,
-    "validation_params": {
-        "payout": {
-            "max": "20000.00"
-        },
-        "stake": {
-            "min": "0.50"
-        }
-    }
-}
 
 */
