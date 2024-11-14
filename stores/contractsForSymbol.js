71a283a7495e859aa20d0fa111a6fe44a0f52167
Aum Bhatt <aum@deriv.com>
2024-11-14 17:29:12 +0400

    feat: temporary logic to aggregate contract types - Will be improved in future

--- stores/contractsForSymbol.js
+++ stores/contractsForSymbol.js
@@ -1,5 +1,53 @@
 import jsonObjectsToBubbleThings from "../utils/jsonObjectsToBubbleThings.js";
 
+const modifyData = (data) => {
+    const result = data.reduce((acc, cur) => {
+        let i = acc.findIndex(item => item.barrier_category === cur.barrier_category && item.contract_category_display === cur.contract_category_display && item.expiry_type === cur.expiry_type && item.sentiment !== cur.sentiment);
+
+        if (i === -1) {
+            acc.push(cur);
+        }
+        return acc;
+
+    }, []).reduce((acc, curr) => {
+        let i = acc.findIndex(item => item.barrier_category === curr.barrier_category && item.contract_category_display === curr.contract_category_display);
+
+        let name = curr.expiry_type;
+        let unit = curr.max_contract_duration.split('').pop();
+        let max = Number(curr.max_contract_duration.split('').slice(0, -1).join(''));
+        let min = Number(curr.min_contract_duration.split('').slice(0, -1).join(''));
+        let expiry = {
+            name, unit, max, min
+        }
+
+
+        if (i === -1) {
+            curr.expiry = [expiry];
+            delete curr.expiry_type;
+            acc.push(curr);
+        }
+
+        else {
+            acc[i].expiry.push(expiry);
+        }
+
+        return acc;
+    }, []).map(item => {
+        if (item.barrier_category === 'euro_atm' && item.contract_category_display === 'Up/Down') {
+            item.contract_category_display = 'Rise/Fall';
+        }
+        return item;
+    });
+
+    let riseFallIndex = result.findIndex(item => item.contract_category_display === 'Rise/Fall');
+    let temp = result[riseFallIndex];
+    result[riseFallIndex] = result[0];
+    result[0] = temp;
+
+
+    return result;
+};
+
 class ContractsForSymbolStore {
     constructor(wsClient) {
         this.wsClient = wsClient;
@@ -19,7 +67,9 @@ class ContractsForSymbolStore {
             const response = await this.wsClient.request(_payload);
 
             if (response) {
-                this.data = response["contracts_for"];
+                let rawData = response["contracts_for"]["available"];
+                let modifiedData = modifyData(rawData);
+                this.data = modifiedData;
                 onDataHandler(jsonObjectsToBubbleThings(this.data));
                 return response;
             }
@@ -30,5 +80,6 @@ class ContractsForSymbolStore {
 
     }
 };
+
 
 export default ContractsForSymbolStore;
