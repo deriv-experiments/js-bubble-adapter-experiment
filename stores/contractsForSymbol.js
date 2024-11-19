5c8d42bd248de6a4d5ce6c3e5161ba6cc2b4ea19
Aum Bhatt <aum@deriv.com>
2024-11-19 12:53:21 +0400

    refactor: contractsForSymbol

--- stores/contractsForSymbol.js
+++ stores/contractsForSymbol.js
@@ -1,76 +1,37 @@
 import jsonObjectsToBubbleThings from "../utils/jsonObjectsToBubbleThings.js";
+import { modifyContracts } from "../utils/modifyContracts.js";
 
-const modifyData = (data) => {
-    const result = data.reduce((acc, cur) => {
-        let i = acc.findIndex(item => item.barrier_category === cur.barrier_category && item.contract_category_display === cur.contract_category_display && item.expiry_type === cur.expiry_type && item.sentiment !== cur.sentiment);
+const getSelectedContractData = (allContracts, selectedBarrierCategory, selectedContractCategory) => {
+    const modifiedContracts = modifyContracts(allContracts);
 
-        if (i === -1) {
-            acc.push(cur);
-        }
-        return acc;
+    const matchingContracts = modifiedContracts.find(contract => contract.barrier_category === selectedBarrierCategory && contract.contract_category === selectedContractCategory);
 
-    }, []).reduce((acc, curr) => {
-        let i = acc.findIndex(item => item.barrier_category === curr.barrier_category && item.contract_category_display === curr.contract_category_display);
-
-        let name = curr.expiry_type;
-        let unit = curr.max_contract_duration.split('').pop();
-        let max = Number(curr.max_contract_duration.split('').slice(0, -1).join(''));
-        let min = Number(curr.min_contract_duration.split('').slice(0, -1).join(''));
-        let expiry = {
-            name, unit, max, min
-        }
-
-
-        if (i === -1) {
-            curr.expiry = [expiry];
-            delete curr.expiry_type;
-            acc.push(curr);
-        }
-
-        else {
-            acc[i].expiry.push(expiry);
-        }
-
-        return acc;
-    }, []).map(item => {
-        if (item.barrier_category === 'euro_atm' && item.contract_category_display === 'Up/Down') {
-            item.contract_category_display = 'Rise/Fall';
-        }
-        return item;
-    });
-
-    let riseFallIndex = result.findIndex(item => item.contract_category_display === 'Rise/Fall');
-    let temp = result[riseFallIndex];
-    result[riseFallIndex] = result[0];
-    result[0] = temp;
-
-
-    return result;
-};
+    return matchingContracts;
+}
 
 class ContractsForSymbolStore {
     constructor(wsClient) {
         this.wsClient = wsClient;
-        this.data = null;
     }
 
-    async request(payload, onDataHandler) {
+    async request(currency, selectedSymbol, selectedBarrierCategory, selectedContractCategory, onDataHandler) {
 
         try {
             await this.wsClient.waitForConnection();
 
             const _payload = {
-                contracts_for: payload.symbol,
-                currency: payload.currency
+                currency: currency,
+                contracts_for: selectedSymbol,
+
             }
 
             const response = await this.wsClient.request(_payload);
 
             if (response) {
-                let rawData = response["contracts_for"]["available"];
-                let modifiedData = modifyData(rawData);
-                this.data = modifiedData;
-                onDataHandler(jsonObjectsToBubbleThings(this.data));
+                let contracts = response["contracts_for"]["available"];
+                const _data = getSelectedContractData(contracts, selectedBarrierCategory, selectedContractCategory);
+                onDataHandler(jsonObjectsToBubbleThings(_data));
+                // onDataHandler(_data);
                 return response;
             }
 
