bad5fdb0abe6d684b9c274c77ecf207852587a5c
Aum Bhatt <aum@deriv.com>
2024-11-20 11:14:51 +0400

    fix: addition of multiple duration types

--- utils/modifyContracts.js
+++ utils/modifyContracts.js
@@ -5,57 +5,64 @@ export const modifyContracts = (data) => {
         let i = acc.findIndex(item => item.barrier_category === curr.barrier_category && item.contract_category === curr.contract_category);
 
         if (i === -1) {
-            const contractData = {
-                ...curr,
-                first: {
+            let expiry = [];
+            if ("expiry_type" in curr) {
+                // aggregate all the available expiry limits into a single array
+                const name = curr.expiry_type;
+                const unit = curr.max_contract_duration.split('').pop();
+                const max = Number(curr.max_contract_duration.split('').slice(0, -1).join(''));
+                const min = Number(curr.min_contract_duration.split('').slice(0, -1).join(''));
+                expiry.push({
+                    name, unit, max, min
+                });
+                delete curr["expiry_type"];
+            }
+
+            const contract_details = [
+                {
                     contract_display: curr.contract_display,
                     contract_type: curr.contract_type,
                     sentiment: curr.sentiment,
                 }
-            };
+            ];
 
-            delete contractData["contract_display"];
-            delete contractData["sentiment"];
+            delete curr["contract_display"];
+            delete curr["sentiment"];
+
+            const contractData = {
+                ...curr,
+                contract_details,
+                expiry
+            };
 
             acc.push(contractData);
         } else {
-            acc[i].second = {
+            if ("expiry_type" in curr) {
+                // aggregate all the available expiry limits into a single array
+                const name = curr.expiry_type;
+                const unit = curr.max_contract_duration.split('').pop();
+                const max = Number(curr.max_contract_duration.split('').slice(0, -1).join(''));
+                const min = Number(curr.min_contract_duration.split('').slice(0, -1).join(''));
+                const expiry = {
+                    name, unit, max, min
+                }
+                delete curr["expiry_type"];
+
+                acc[i].expiry.push(expiry)
+            }
+
+            acc[i].contract_details.push({
                 contract_display: curr.contract_display,
                 contract_type: curr.contract_type,
                 sentiment: curr.sentiment,
-            };
+            });
         }
 
         return acc;
 
     }, [])
 
-    const combinedContractsWithExpiry = combinedContracts[0].max_contract_duration ? combinedContracts.reduce((acc, curr) => {
-        let i = acc.findIndex(item => item.barrier_category === curr.barrier_category && item.contract_category === curr.contract_category);
-
-        let name = curr.expiry_type;
-        let unit = curr.max_contract_duration.split('').pop();
-        let max = Number(curr.max_contract_duration.split('').slice(0, -1).join(''));
-        let min = Number(curr.min_contract_duration.split('').slice(0, -1).join(''));
-        let expiry = {
-            name, unit, max, min
-        }
-
-
-        if (i === -1) {
-            curr.expiry = [expiry];
-            delete curr.expiry_type;
-            acc.push(curr);
-        }
-
-        else {
-            acc[i].expiry.push(expiry);
-        }
-
-        return acc;
-    }, []) : null;
-
-    const modifiedContracts = (combinedContractsWithExpiry ?? combinedContracts).map(item => {
+    const modifiedContracts = combinedContracts.map(item => {
         if (item.contract_type in tradeConfig) {
             item = {
                 ...item,
